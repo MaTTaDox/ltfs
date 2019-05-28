b8e1f8e1cf697fa85ab8610ff74c46d91953be8b
Dominik Malsch <dominik.malsch@plentymarkets.com>
2019-05-28 23:15:33 +0200

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -3,7 +3,7 @@ sudo su <<EOF
 wget -q -O- https://packages.sury.org/php/apt.gpg | apt-key add -
 echo "deb https://packages.sury.org/php/ stretch main" | tee /etc/apt/sources.list.d/php.list
 apt-get update
-apt-get install -y supervisor git php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-xml php7.2-fpm nginx unzip
+apt-get install -y supervisor git php7.2 php7.2-curl php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-xml php7.2-fpm nginx unzip
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 cd /var/www
